370fce21f2f11cc86be7b2fd4450aa9f151c311f
Malcolm <102746333+malcolms10@users.noreply.github.com>
2024-07-19 21:00:38 +0100

    Update ProfileCard.js

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,7 +1,22 @@
-export default function profileCard(params) {
+export default function ProfileCard({title, handle, desc, image}) {
     return(
-        <>
-            Profile Card
-        </>
+    
+            <div className="card">
+                <div className="card-image">
+                    <figure className="image is-1by1">
+                        <img src={image} alt="pdas logo"/>
+                    </figure>
+                </div>
+                <div className="card-content">
+                    <div className="media-content">
+                        <p className="title is-4">{title}</p> 
+                        <p className="subtitle is-6">{handle}</p>
+                    </div>
+
+                    <div className="content">{desc}</div>
+                   
+                </div>
+                
+            </div>
     )
 }
